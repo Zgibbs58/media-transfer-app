a5d2925487611e477b0fb6bab9104562e733dd8d
Zach Gibbs <zacharywgibbs@gmail.com>
2024-08-16 13:50:10 -0500

    Set up the settings page to only show for logged in user and allow the update of the username.

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,12 +1,20 @@
 import { Metadata } from "next";
 import SettingsPage from "./SettingsPage";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Settings",
 };
 
 export default async function Page() {
-  // TODO: Protect this page via authentication
+  const session = await auth();
+  const user = session?.user;
 
-  return <SettingsPage />;
+  //if not logged in, redirect to signin page
+  if (!user) {
+    redirect("/api/auth/signin?callbackUrl=/settings");
+  }
+
+  return <SettingsPage user={user} />;
 }
