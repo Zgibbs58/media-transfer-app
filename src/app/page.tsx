bfdb46be53dc8f95137536a7af018828548d9349
Zach Gibbs <zacharywgibbs@gmail.com>
2024-08-21 11:33:44 -0500

    updated the schema to store files updated by users and changed the routing of the home page to redirect to the user page, once logged in.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 import prisma from "@/lib/prisma";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import getSession from "@/lib/getSession";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
+  // const users = await prisma.user.findMany();
+  const session = await getSession();
+  const user = session?.user;
 
   return (
     <main className="flex flex-col items-center gap-6 px-3 py-10">
       <h1 className="text-center text-4xl font-bold">
         Welcome to the Media Transfer App
       </h1>
-      <p></p>
+      {user && redirect(`/user/${user.id}`)}
     </main>
   );
 }
